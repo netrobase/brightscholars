58cf7a670d5abe84482c466a54bd61d3e5c04c07
netrobase <netrobase@gmail.com>
2024-11-10 16:41:41 +0100

    Add Features Section

--- src/components/FeatureBox.tsx
+++ src/components/FeatureBox.tsx
@@ -11,10 +11,19 @@ interface BoxProps {
 const Box: React.FC<BoxProps> = ({ image, title, description }) => {
   return (
     <div className=" p-8 items-center gap-6 border rounded-lg shadow-lg hover:shadow-2xl transition-all">
-      <div className=" w-32 h-32 bg-gray-200 rounded-full">
-        <Image src={image} alt={title} width={100}  height={100} className="w-full h-full object-cover rounded-full" />
-      </div>
-      <div className="">
+      <div className="flex items-center justify-center ">
+     <div className="w-32 h-32 bg-gray-200 rounded-full">
+       <Image 
+        src={image} 
+        alt={title} 
+        width={100} 
+        height={100} 
+      className="w-full h-full object-cover rounded-full" 
+    />
+  </div>
+</div>
+
+      <div className="text-center">
         <h4 className="text-xl font-bold">{title}</h4>
         <p className="text-sm text-gray-600">{description}</p>
       </div>
